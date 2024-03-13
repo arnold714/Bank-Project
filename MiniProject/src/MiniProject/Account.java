bf1b87870ca5f94d6f2fd8005ec3d4d45ec84dd5
Kosta <Kosta@DESKTOP-1JJD3LM>
2024-03-13 16:22:18 +0900

    --진하경 #01

---
+++ MiniProject/src/MiniProject/Account.java
@@ -0,0 +1,12 @@
+package MiniProject;
+
+import java.sql.Date;
+
+class Account {
+	private int account_num;
+	private String id;
+	private int balance;
+	private Date date;
+	
+	//적금 만기일,이율 추가
+}
---
+++ MiniProject/src/MiniProject/DaoParent.java
@@ -0,0 +1,40 @@
+//package MiniProject;
+//
+//import java.util.ArrayList;
+//
+//class DaoParent {
+//	private Account account;
+//	
+//	public DaoParent() {
+//		account = new Account();
+//	}
+//	//insert
+//	//계좌 추가(상품 가입)
+//	public void addAccount(Account account) {
+//		
+//	}
+//		
+//		//select
+//		//전체 계좌 조회
+//		public ArrayList<Vo> SelectAll(){
+//		}
+//		//계좌번호로 조회
+//		public vo selectByNum(int num) {
+//		}
+//		
+//		//update
+//		//입금(입금 금액 추가)
+//		public void deposit(vo) {
+//			//객체 받아와서 추가 입금(날짜에 맞춰서)
+//		}
+//		//적금 조회시 기간이 만기면 이자 추가하기 이후 서비스에서 금액이 0되면 탈퇴 가능
+//		public void	expire(vo) {
+//			//객체 받아와서 sysdate와 날짜 비교해서 만기시 총 금액에 이자율 더하기
+//		}
+//		
+//		//delete
+//		//계좌 삭제(탈회)(금액이 0일 경우)
+//		public void delete(int num){
+//			//계좌번호에 맞는거 탈퇴하기
+//		}
+//}
