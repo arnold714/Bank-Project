7d276c46df229c68aba181f727bbf7cdd402c55c
arnold714 <arnold714@naver.com>
2024-03-18 09:31:20 +0900

    전부 merge

--- MiniProject/src/MiniProject/Record/Record.java
+++ MiniProject/src/MiniProject/Record/Record.java
@@ -3,24 +3,38 @@ package MiniProject.Record;
 import java.sql.Date;
 
 public class Record {
-	private int num;
+	//레코드 번호
+	private int num; 
+	//계좌번호
 	private int account_num;
+	//입금,출금액
 	private int money;
-	private String name;
-	private Date newDate;
+	//입금,출금자명
+	private String name; 
+	//금액(잔액)
+	private int balnace;
+	//거래일
+	private Date newDate; 
+	//거래 구분: 예금 or 출금
+	private int isDeposit; //1이면 입금 ,2이면 출금
 	
+
+
 	public Record() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public Record(int num, int account_num, int money, String name, Date newDate) {
+
+	public Record(int num, int account_num, int money, String name, int balnace, Date newDate, int isDeposit) {
 		super();
 		this.num = num;
 		this.account_num = account_num;
 		this.money = money;
 		this.name = name;
+		this.balnace = balnace;
 		this.newDate = newDate;
+		this.isDeposit = isDeposit;
 	}
 
 	public int getNum() {
@@ -55,6 +69,14 @@ public class Record {
 		this.name = name;
 	}
 
+	public int getBalnace() {
+		return balnace;
+	}
+
+	public void setBalnace(int balnace) {
+		this.balnace = balnace;
+	}
+
 	public Date getNewDate() {
 		return newDate;
 	}
@@ -62,12 +84,23 @@ public class Record {
 	public void setNewDate(Date newDate) {
 		this.newDate = newDate;
 	}
+	
+	public int getIsDeposit() {
+		return isDeposit;
+	}
+
+	public void setIsDeposit(int isDeposit) {
+		this.isDeposit = isDeposit;
+	}
+	
 
 	@Override
 	public String toString() {
 		return "Record [num=" + num + ", account_num=" + account_num + ", money=" + money + ", name=" + name
-				+ ", newDate=" + newDate + "]";
+				+ ", balnace=" + balnace + ", newDate=" + newDate + ", isDeposit=" + isDeposit + "]";
 	}
+
+	
 	
 	
 
